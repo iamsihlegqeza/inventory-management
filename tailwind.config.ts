a1c1843a471cf7f26967618e2c7113b7c4233951
Sihle Gqeza <164931114+iamsihlegqeza@users.noreply.github.com>
2025-08-12 12:45:20 +0200

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,15 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+        },
+        colors:{
+          lamaSky: "#C3EBFA",
+          lamaSkyLight: "#EDF9FD",
+          lamaPurple: "#CFCEFF",
+          lamaPurpleLight: "#F1F0FF",
+          lamaYellow: "#FAE27C",
+          lamaYellowLight: "#FEFCE8",
+        }
     },
   },
   plugins: [],
